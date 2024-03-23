7f31b5aa14a55ac4e056821d9a033095b4ac86b1
woaitsAryan <yoprince.ab@gmail.com>
2024-03-23 12:38:22 +0530

    feat: added seat state

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,17 @@ import { useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { useRouter } from "next/navigation";
+
+
 export default function Home() {
   const [regNo, setRegNo] = useState("");
-  const [response, setResponse] = useState(null);
+  const [seat, setSeat] = useState<null | string>(null);
   const router=useRouter()
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     try {
       const res = await axios.post("/api/book", { regNo });
-      setResponse(res.data);
+      setSeat(res.data.seat);
       setRegNo("");
       router.push("/result")
     } catch (error: any) {
@@ -30,7 +32,7 @@ export default function Home() {
         />
         <button type="submit">Submit</button>
       </form>
-      {response && <div>{JSON.stringify(response)}</div>}
+      {seat && <div>{JSON.stringify(seat)}</div>}
     </main>
   );
 }
