72b7b4aa22b498a645b7b86fdf6cc0571d1b215e
manaslaud <manaslaud@hotmail.com>
2024-03-23 12:36:50 +0530

    feat:routing

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 'use client'
-
 import { useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
-
+import { useRouter } from "next/navigation";
 export default function Home() {
   const [regNo, setRegNo] = useState("");
   const [response, setResponse] = useState(null);
-
+  const router=useRouter()
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     try {
       const res = await axios.post("/api/book", { regNo });
       setResponse(res.data);
       setRegNo("");
+      router.push("/result")
     } catch (error: any) {
       toast.error(error.response.data.message)
     }
